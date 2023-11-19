cb94c22f7ea1022e0166176d0ed2d1ba57c1dfda
Kathy Lee <kathyam.lee@mail.utoronto.ca>
2023-11-18 19:10:11 -0500

    added sort button

--- penny/lib/screens/search_page.dart
+++ penny/lib/screens/search_page.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:penny/widgets/home_nav_bar.dart';
 import 'package:penny/widgets/product.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   final String inputText;
 
   SearchPage({required this.inputText});
 
   @override
+  _SearchPageState createState() => _SearchPageState(inputText: inputText);
+}
+
+class _SearchPageState extends State<SearchPage> {
+  String inputText;
+  String selectedSortOption;
+
+  _SearchPageState({required this.inputText})
+      : selectedSortOption = 'Lowest to Highest Price';
+
+  final TextEditingController _controller = TextEditingController();
+
+  // Define sorting options here
+  List<String> sortOptions = [
+    'Lowest to Highest Price',
+    'Highest to Lowest Price',
+    'By Name'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = inputText;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController _controller =
-        TextEditingController(text: inputText);
+    // Existing build method...
+
     return Scaffold(
       body: Stack(
         children: [
@@ -27,7 +54,7 @@ class SearchPage extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 60), // Adjust based on AppBar height
+            padding: EdgeInsets.only(top: 60),
             child: ListView(
               padding: EdgeInsets.all(20),
               children: [
@@ -61,12 +88,14 @@ class SearchPage extends StatelessWidget {
                           decoration: InputDecoration(
                             suffixIcon: InkWell(
                               onTap: () {
-                                // Navigate to the SearchPage when the search icon is clicked
+                                // navigate to the SearchPage when the search icon is clicked
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => SearchPage(
-                                          inputText: _controller.text)),
+                                    builder: (context) => SearchPage(
+                                      inputText: _controller.text,
+                                    ),
+                                  ),
                                 );
                               },
                               child: Icon(Icons.search, color: Colors.amber),
@@ -80,6 +109,69 @@ class SearchPage extends StatelessWidget {
                       SizedBox(height: 40),
 
                       // items widget!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '7 results found',
+                            style: GoogleFonts.phudu(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: Container(
+                              padding: EdgeInsets.only(left: 16, right: 16),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                // border:
+                                //     Border.all(color: Colors.grey, width: 1),
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.3),
+                                    spreadRadius: 2,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              child: DropdownButton<String>(
+                                dropdownColor: Colors.white,
+                                icon: Icon(Icons.sort, color: Colors.amber),
+                                value: selectedSortOption,
+                                onChanged: (String? newValue) {
+                                  if (newValue != null) {
+                                    setState(() {
+                                      selectedSortOption = newValue;
+                                      // TODO: Add sorting logic
+                                    });
+                                  }
+                                },
+                                items: sortOptions.map((String option) {
+                                  return DropdownMenuItem<String>(
+                                    value: option,
+                                    child: Row(
+                                      children: [
+                                        Text(option,
+                                            style:
+                                                TextStyle(color: Colors.black)),
+                                        SizedBox(width: 8),
+                                      ],
+                                    ),
+                                  );
+                                }).toList(),
+                                underline: SizedBox(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+
+                      // items widget
                       ProductWidget(path: "straw"),
                     ],
                   ),
