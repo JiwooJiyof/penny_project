4144be26b726fbb8949bbfa69b1426b30edd7dcf
dalshekerchi <dalshekerchi@mail.utoronto.com>
2023-11-18 22:49:00 -0500

    Created a user profile page

--- penny/lib/screens/home_page.dart
+++ penny/lib/screens/home_page.dart
@@ -7,42 +7,27 @@ import 'package:penny/screens/search_page.dart';
 
 class HomePage extends StatelessWidget {
   final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: HomeNavBar(),
-          ),
-          Positioned.fill(
-            child: Image.asset(
-              'assets/main_page.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 60), // Adjust based on AppBar height
-            child: ListView(
-              padding: EdgeInsets.all(20),
+          HomeNavBar(), // Stationary AppBar
+          Expanded(
+            child: Stack(
               children: [
-                // HomeNavBar(),
-                SizedBox(height: 40),
-                Container(
-                  // temp height
-                  padding: EdgeInsets.only(top: 15),
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    // borderRadius: BorderRadius.only(
-                    //   topLeft: Radius.circular(35),
-                    //   topRight: Radius.circular(35),
-                    // ),
+                Positioned.fill(
+                  child: Image.asset(
+                    'assets/main_page.png',
+                    fit: BoxFit.cover,
                   ),
+                ),
+                SingleChildScrollView(
+                  padding: EdgeInsets.only(top: 20),
                   child: Column(
                     children: [
+                      SizedBox(height: 40),
                       Align(
                         alignment: Alignment.center,
                         child: Text(
@@ -54,7 +39,6 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 50),
-                      // search bar!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                       Container(
                         margin: EdgeInsets.symmetric(horizontal: 100),
                         padding: EdgeInsets.symmetric(horizontal: 15),
@@ -94,14 +78,10 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 80),
-                      // categories!!!!!!!!!!!!!!!!!!
                       CategoriesWidget(),
-
-                      // items!!!!!!!!!!!!
                       Container(
                         alignment: Alignment.centerLeft,
-                        margin:
-                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
                         child: Text(
                           "Today's Prices",
                           style: GoogleFonts.phudu(
@@ -111,8 +91,6 @@ class HomePage extends StatelessWidget {
                           ),
                         ),
                       ),
-
-                      // items widget!!!!!!!!!!!!!!!!!!!!!!!!!!!
                       ProductWidget(path: ""),
                     ],
                   ),
---
+++ penny/lib/screens/user_profile.dart
@@ -0,0 +1,89 @@
+// hello_world_dialog.dart
+
+import 'package:flutter/material.dart';
+
+class HelloWorldDialog extends StatelessWidget {
+  final String address = "27 King's College Cir, Toronto, ON M5S 1A1"; // Default address
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8, // Dialog width is 80% of screen width
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () => Navigator.of(context).pop(), // Close the dialog
+              ),
+            ),
+            CircleAvatar(
+              radius: 40, // Size of Avatar
+              backgroundColor: Colors.blueAccent,
+              child: Icon(
+                Icons.person,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Bob Smith',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () {
+                // Implement change name functionality
+              },
+              child: Text('Change name'),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'bob.smith@gmail.com',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () {
+                // Implement change email functionality
+              },
+              child: Text('Change email'),
+            ),
+            SizedBox(height: 10),
+            Text(
+              address,
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () {
+                // Implement change address functionality
+              },
+              child: Text('Change address'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '••••••••',
+              style: TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () {
+                // Implement change password functionality
+              },
+              child: Text('Change password'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
