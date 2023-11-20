216993268d4d7a1f42215734f40416fc5d3eddb0
dalshekerchi <dalshekerchi@mail.utoronto.com>
2023-11-20 03:10:59 -0500

    Moved location logic into a file

--- penny/lib/main.dart
+++ penny/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:penny/screens/home_page.dart';
 import 'package:penny/screens/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:location/location.dart';
+import 'package:penny/widgets/location_utils.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +11,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final Future<LocationData?> locationFuture = getCurrentLocation();
+  final Future<LocationData?> locationFuture = LocationUtils.getCurrentLocation(); // Use the LocationUtils class
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +29,9 @@ class MyApp extends StatelessWidget {
         "/": (context) => FutureBuilder<LocationData?>(
               future: locationFuture,
               builder: (context, snapshot) {
-                // Check if the future is complete and has data
                 if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
                   return HomePage(locationData: snapshot.data);
                 }
-                // Return HomePage with null locationData if the future is not complete or has no data
                 return HomePage(locationData: null);
               },
             )
@@ -40,35 +39,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-Future<LocationData?> getCurrentLocation() async {
-  Location location = new Location();
-
-  bool _serviceEnabled;
-  PermissionStatus _permissionGranted;
-  LocationData _locationData;
-
-  _serviceEnabled = await location.serviceEnabled();
-  if (!_serviceEnabled) {
-    _serviceEnabled = await location.requestService();
-    if (!_serviceEnabled) {
-      return null;
-    }
-  }
-
-  _permissionGranted = await location.hasPermission();
-  if (_permissionGranted == PermissionStatus.denied) {
-    _permissionGranted = await location.requestPermission();
-    if (_permissionGranted != PermissionStatus.granted) {
-      return null;
-    }
-  }
-
-  _locationData = await location.getLocation();
-  return _locationData;
-}
-
-// You can uncomment and modify this function to send the location data to your backend
-// Future<void> sendLocationToBackend(LocationData locationData) async {
-//   // Your implementation to send location data to backend
-// }
