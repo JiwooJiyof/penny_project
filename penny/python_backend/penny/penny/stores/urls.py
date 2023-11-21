e3e45751e224eb63ac8af6b35f8d4054b37a5f8e
Prithee Roy <prithee.roy@mail.utoronto.ca>
2023-11-21 15:16:55 -0500

    added functionality to search for nearby stores

--- penny/python_backend/penny/penny/stores/urls.py
+++ penny/python_backend/penny/penny/stores/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import StoreView, ItemUpdateView
+from .views import StoreView, ItemUpdateView, get_stores_distance
 
 urlpatterns = [
     path('', StoreView.as_view(), name='store-view'),
     path('<int:pk>/',
          ItemUpdateView.as_view(), name='item-update'),
+    path('distance/', get_stores_distance, name='get_stores_distance')
 ]
--- penny/python_backend/penny/penny/stores/views.py
+++ penny/python_backend/penny/penny/stores/views.py
@@ -13,7 +13,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from rest_framework import status
-
+from django.conf import settings
+import requests
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
@@ -67,3 +68,47 @@ class StoreView(ListAPIView):
             queryset = queryset.filter(name__icontains=search_term)
 
         return queryset
+    
+def get_stores_distance(request):
+    user_location = request.GET.get('user_location')
+    google_api_key = 'AIzaSyDpIWKiBj1P0x5buBX2losmBknSRYn1HVI'
+
+    # Supabase REST URL and Headers
+    supabase_url = 'https://ytjttwkyfkltqqxpdaox.supabase.co/rest/v1/stores_store'
+    supabase_headers = {
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0anR0d2t5ZmtsdHFxeHBkYW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1MzA2NDQsImV4cCI6MjAxNjEwNjY0NH0.BJxaaSqeCJDp2vKOMj2UNW5YwytqezaLfez24kPMuZs',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0anR0d2t5ZmtsdHFxeHBkYW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1MzA2NDQsImV4cCI6MjAxNjEwNjY0NH0.BJxaaSqeCJDp2vKOMj2UNW5YwytqezaLfez24kPMuZs'
+    }
+
+    # Fetch stores from Supabase
+    stores_response = requests.get(supabase_url, headers=supabase_headers)
+    if stores_response.status_code != 200:
+        return JsonResponse({'error': 'Failed to fetch data from Supabase'}, status=500)
+
+    stores = stores_response.json()
+    destinations = '|'.join([store['location'] for store in stores])
+
+    # Google Maps API to calculate distances
+    maps_response = requests.get(
+        'https://maps.googleapis.com/maps/api/distancematrix/json',
+        params={
+            'origins': user_location,
+            'destinations': destinations,
+            'key': google_api_key
+        }
+    )
+
+    if maps_response.status_code == 200:
+        elements = maps_response.json().get('rows', [])[0].get('elements', [])
+        store_distances = []
+        for store, element in zip(stores, elements):
+            if element['status'] == 'OK':
+                distance_text = element['distance']['text']
+                store_distances.append({
+                    'name': store['name'],
+                    'distance': distance_text
+                })
+        print(store_distances)
+        return JsonResponse(store_distances, safe=False)
+    else:
+        return JsonResponse({'error': 'Failed to fetch distances'}, status=500)
