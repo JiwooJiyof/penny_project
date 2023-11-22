92f4d8902a2a550379e2e3b3a4b3a834371ecd54
dravgav <74106672+dravgav@users.noreply.github.com>
2023-11-21 21:00:21 -0500

    combined check views

--- penny/python_backend/penny/penny/shoppingcart/urls.py
+++ penny/python_backend/penny/penny/shoppingcart/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('addItem/<str:item_name>/', views.AddItemToCartView.as_view(), name='addItem'),
     path('removeItem/<str:item_name>/', views.RemoveItemFromCartView.as_view(), name='removeItem'),
-    path('check/<str:item_name>/', views.CheckOffItemView.as_view(), name='check'),
-    path('uncheck/<str:item_name>/', views.UncheckItemView.as_view(), name='uncheck'),
+    path('togglecheck/<str:item_name>/', views.ToggleCheckItemView.as_view(), name='togglecheck'),
     path('view/', views.ViewItems.as_view(), name='view'),
 ]
