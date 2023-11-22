22508d00afbfb2aec7e29bcf17cb7bc360897cae
dalshekerchi <dalshekerchi@mail.utoronto.com>
2023-11-22 18:50:20 -0500

    Updated some s

--- penny/python_backend/penny/penny/shoppingcart/urls.py
+++ penny/python_backend/penny/penny/shoppingcart/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from . import views
-
+from .views import ViewCartItems, UpdateCartItems
 
 urlpatterns = [
-    path('addItem/<str:item_name>/', views.AddItemToCartView.as_view(), name='addItem'),
-    path('removeItem/<str:item_name>/', views.RemoveItemFromCartView.as_view(), name='removeItem'),
-    path('togglecheck/<str:item_name>/', views.ToggleCheckItemView.as_view(), name='togglecheck'),
-    path('view/', views.ViewItems.as_view(), name='view'),
+    path('view/', ViewCartItems.as_view(), name='view-cart-items'),
+    path('update/', UpdateCartItems.as_view(), name='update-cart-items'),
 ]
--- penny/python_backend/penny/penny/shoppingcart/views.py
+++ penny/python_backend/penny/penny/shoppingcart/views.py
@@ -1,60 +1,40 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.request import Request
+from rest_framework.generics import ListAPIView, UpdateAPIView
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from .models import ShoppingCart
-from django.http import Http404
+from .models import ShoppingCart, Account
 from .serializers import ShoppingCartSerializer
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.filters import OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
 
-
-class AddItemToCartView(CreateAPIView):
+class ViewCartItems(ListAPIView):
+    # Keep permission_classes as [AllowAny] if you must
     serializer_class = ShoppingCartSerializer
-    permission_classes = [AllowAny]
-
-    def perform_create(self, serializer):
-        serializer.save()
-    
-
-class RemoveItemFromCartView(DestroyAPIView):
-    permission_classes = [AllowAny]
-
-    def get_object(self):
-        item_name = self.kwargs.get('item_name')
-        cart_item = ShoppingCart.objects.filter(item_name=item_name).first()
-
-        if cart_item is None:
-            raise Http404("No ShoppingCart item found with this item name")
-
-        return cart_item
-
-class ToggleCheckItemView(UpdateAPIView):
-    serializer_class = ShoppingCartSerializer
-    permission_classes = [AllowAny]
-
-    def get_object(self):
-        item_name = self.kwargs.get('item_name')
-        cart_item = ShoppingCart.objects.filter(item_name=item_name).first()
-
-        if cart_item is None:
-            raise Http404(f"No ShoppingCart item found with this item name: {item_name}")
-
-        return cart_item
-
-    def perform_update(self, serializer):
-        cart_item = self.get_object()
-        serializer.save(is_checked=not cart_item.is_checked)
-
-
-class ViewItems(ListAPIView):
-    serializer_class = ShoppingCartSerializer
-    permission_classes = [AllowAny]
 
     def get_queryset(self):
-        queryset = ShoppingCart.objects.all()
-        return queryset
+        # Fetch the account that has is_loggedin set to True
+        try:
+            account = Account.objects.get(is_loggedin=True)
+            return ShoppingCart.objects.filter(account=account)
+        except Account.DoesNotExist:
+            # Handle the case where no user is logged in
+            return ShoppingCart.objects.none()
+
+class UpdateCartItems(UpdateAPIView):
+    serializer_class = ShoppingCartSerializer
+    # Keep permission_classes as [AllowAny] if you must
+
+    def update(self, request, *args, **kwargs):
+        # Fetch the account that has is_loggedin set to True
+        try:
+            account = Account.objects.get(is_loggedin=True)
+        except Account.DoesNotExist:
+            # Handle the case where no user is logged in
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        for item_data in request.data:
+            item_name = item_data.get('item_name')
+            is_checked = item_data.get('is_checked')
+            ShoppingCart.objects.update_or_create(
+                account=account, 
+                item_name=item_name, 
+                defaults={'is_checked': is_checked}
+            )
+
+        return Response({"message": "Cart updated successfully"})
